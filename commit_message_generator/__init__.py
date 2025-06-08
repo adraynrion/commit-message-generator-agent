1ff2aea25081e4bd5d80dcd642b0259f46bfe86f
adraynrion <grondin.florian@protonmail.com>
2025-06-08 11:28:45 +0200

    VERSION: Upgrade version to 1.1.2

--- commit_message_generator/__init__.py
+++ commit_message_generator/__init__.py
@@ -22,7 +22,7 @@ from .git_utils import (
 )
 from .models import CommitAnalysis, CommitMessageResponse
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __all__ = [
     # Main classes
     "CommitMessageGenerator",
